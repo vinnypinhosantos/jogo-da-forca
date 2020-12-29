5ee4072ea04a73d12f57c64eee1af191fc1967b2
vinnypinhosantos <76694892+vinnypinhosantos@users.noreply.github.com>
2020-12-29 15:14:02 -0300

    Comentários
    Adicionei comentários que explicam o código.

--- forca.py
+++ forca.py
@@ -1,6 +1,7 @@
 import random
 from time import sleep
 
+# Escolhe aleatoriamente a palavra de uma das categorias
 categorias = ["animais", "objetos", "paises", "times"]
 categoria = random.choice(categorias)
 
@@ -9,15 +10,19 @@ with open(f'{categoria}.txt', 'r') as f:
 
 palavra = random.choice(palavras)[:-1]
 
+# Define o número máximo de erros e uma variavel para terminar o jogo
 erros_permitidos = 6
 acabou = False
 chutes = []
 
 print(f"Categoria: {categoria.capitalize()}")
 
+# Lopping principal do jogo
 while not acabou:
-
+	
+	# Percorre a palvra e escreve os espaços e letras
 	for letra in palavra:
+		# Escreve os traços e espaços
 		if letra == "-":
 			print("-", end='')
 			chutes.append(letra)
@@ -25,24 +30,30 @@ while not acabou:
 			print(" ", end='')
 			chutes.append(letra)
 		else:
+			# Verifica se a letra "chutada" está na palavra
 			if letra.lower() in chutes:
 				print(f"{letra}", end=' ')
 			else:
 				print("_", end=' ')
 	print()
+	
+	# Pede a letra ao usuário e adiciona na lista de chutes
 	chute = input(f"Você ainda pode errar {erros_permitidos} vezes. Seu palpite: ")
 	chutes.append(chute)
 	
+	# Verifica se o chute não está na palavra e tira uma chance do usuário até chegar a 0
 	if chute.lower() not in palavra.lower():
 		erros_permitidos -= 1
 		if erros_permitidos == 0:
 			break
-
+	
+	# Se o usuário acertou o loop acaba, se não o loop recomeça
 	acabou = True
 	for letra in palavra:
 		if letra.lower() not in chutes:
 			acabou = False
 
+# Mostra se o usuário acertou ou não
 if acabou:
 	print(f"Você acertou! A palavra era {palavra}")
 else:
