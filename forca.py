f30a1b54625acafee7121b4655e96ef993fc5cc7
Vinicius de Pinho Santos <vinnypinhosantos@gmail.com>
2020-12-29 21:10:59 -0300

    Modularização
    Criei um módulo com funções para facilitar a manutenção do código.

--- forca.py
+++ forca.py
@@ -1,77 +1,21 @@
 import random
 from time import sleep
+import funcoes
 
 while True:
-	# Escolhe aleatoriamente a palavra de uma das categorias
-	categoria = random.choice(["animais", "objetos", "paises", "times"])
+	
+	# Escolha de uma palavra aleatória
+	palavra = funcoes.escolhe_palavra()
 
-	with open(f'{categoria}.txt', 'r') as f:
-		palavras = f.readlines()
-
-	palavra = random.choice(palavras)[:-1]
-
-	# Define o número máximo de erros e uma variavel para terminar o jogo
+	# Define o número máximo de erros, a lista de chites e uma variavel para terminar o jogo
 	erros_permitidos = 6
 	acabou = False
 	chutes = []
 
-	print(f"Categoria: {categoria.capitalize()}")
-
-	# Loop principal do jogo
-	while not acabou:
-		
-		# Percorre a palvra e escreve os espaços e letras
-		for letra in palavra:
-			# Escreve os traços e espaços
-			if letra == "-":
-				print("-", end='')
-				chutes.append(letra)
-			elif letra == " ":
-				print("  ", end='')
-				chutes.append(letra)
-			else:
-				# Verifica se a letra "chutada" está na palavra
-				if letra.lower() in chutes:
-					print(f"{letra}", end=' ')
-				else:
-					print("_", end=' ')
-		print()
-		
-		# Pede a letra ao usuário e adiciona na lista de chutes
-		chute = input(f"Você ainda pode errar {erros_permitidos} vezes. Seu palpite: ")
-		chutes.append(chute)
-		
-		# Verifica se o chute não está na palavra e tira uma chance do usuário até chegar a 0
-		if chute.lower() not in palavra.lower():
-			erros_permitidos -= 1
-			if erros_permitidos == 0:
-				break
-		
-		# Se o usuário acertou o loop acaba, se não o loop recomeça
-		acabou = True
-		for letra in palavra:
-			if letra.lower() not in chutes:
-				acabou = False
-
-	# Mostra se o usuário acertou ou não
-	if acabou:
-		print(f"Você acertou! A palavra era {palavra}")
-	else:
-		print(f"Game Over! A palavra era {palavra}")
-
-	# Verifica se o usuário quer jogar de novo
-	while True:
-		print("-" * 20)
-		novamente = input("Quer jogar de novo? [S/N]").upper()
-		print("-" * 20)
-		if novamente == "N":
-			acabou = True
-			break
-		elif novamente == "S":
-			acabou = False
-			break
-		else:
-			print("Digite S ou N para escolher!")
+	# Executa o jogo
+	acabou = funcoes.jogo(palavra, acabou, chutes, erros_permitidos)
+	funcoes.verifica(palavra, acabou)
+	novamente = funcoes.de_novo(acabou)
 	if novamente == "N":
 		break
 
---
+++ funcoes.py
@@ -0,0 +1,80 @@
+import random
+
+
+def escolhe_palavra():
+	# Escolhe aleatoriamente a palavra de uma das categorias
+	categoria = random.choice(["animais", "objetos", "paises", "times"])
+
+	with open(f'{categoria}.txt', 'r') as f:
+		palavras = f.readlines()
+
+	palavra = random.choice(palavras)[:-1]
+
+	print(f"Categoria: {categoria.capitalize()}")
+
+	return palavra
+
+
+def jogo(palavra, acabou, chutes, erros_permitidos):
+	while not acabou:
+		
+		# Percorre a palvra e escreve os espaços e letras
+		for letra in palavra:
+			# Escreve os traços e espaços
+			if letra == "-":
+				print("-", end='')
+				chutes.append(letra)
+			elif letra == " ":
+				print("  ", end='')
+				chutes.append(letra)
+			else:
+				# Verifica se a letra "chutada" está na palavra
+				if letra.lower() in chutes:
+					print(f"{letra}", end=' ')
+				else:
+					print("_", end=' ')
+		print()
+		
+		# Pede a letra ao usuário e adiciona na lista de chutes
+		chute = input(f"Você ainda pode errar {erros_permitidos} vezes. Seu palpite: ")
+		chutes.append(chute)
+		
+		# Verifica se o chute não está na palavra e tira uma chance do usuário até chegar a 0
+		if chute.lower() not in palavra.lower():
+			erros_permitidos -= 1
+			if erros_permitidos == 0:
+				break
+		
+		# Se o usuário acertou o loop acaba, se não o loop recomeça
+		acabou = True
+		for letra in palavra:
+			if letra.lower() not in chutes:
+				acabou = False
+	return acabou
+
+
+def verifica(palavra, acabou):
+	# Mostra se o usuário acertou ou não
+	if acabou:
+		print("-" * 20)
+		print(f"Você acertou! A palavra era {palavra}")
+	else:
+		print("-" * 20)
+		print(f"Game Over! A palavra era {palavra}")
+
+
+def de_novo(acabou):
+	# Verifica se o usuário quer jogar de novo
+	while True:
+		print("-" * 20)
+		novamente = input("Quer jogar de novo? [S/N]").upper()
+		print("-" * 20)
+		if novamente == "N":
+			acabou = True
+			break
+		elif novamente == "S":
+			acabou = False
+			break
+		else:
+			print("Digite S ou N para escolher!")
+	return novamente
